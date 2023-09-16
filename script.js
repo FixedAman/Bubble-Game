ed4b979c3112a722f89d0327b94e8bff0f2bb4d5
aman mahish <amanmahish73@gmail.com>
2023-09-16 04:22:50 -0600

    bubble changing

---
+++ script.js
@@ -0,0 +1,6 @@
+clutter = "" ;
+for (var i = 1 ; i <=133 ; i++){
+    var  random = Math.floor(Math.random()*10)
+    clutter += ` <div class="bubble">${random}</div>`
+}
+document.querySelector("#panel2").innerHTML = clutter ;
