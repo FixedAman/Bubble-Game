d46f50eb87785d7219a2972d2708ba7bf98837c2
aman mahish <amanmahish73@gmail.com>
2023-09-16 04:25:29 -0600

    java script

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-clutter = "" ;
-for (var i = 1 ; i <=133 ; i++){
-    var  random = Math.floor(Math.random()*10)
-    clutter += ` <div class="bubble">${random}</div>`
+function bubblestart(){
+    clutter = "" ;
+    for (var i = 1 ; i <=133 ; i++){
+        var  random = Math.floor(Math.random()*10)
+        clutter += ` <div class="bubble">${random}</div>`
+    }
+    document.querySelector("#panel2").innerHTML = clutter ;
 }
-document.querySelector("#panel2").innerHTML = clutter ;
+bubblestart();
